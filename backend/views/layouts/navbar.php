df107f81e5be2653f68dc2791574e45bfb3bfbb7
Administrator <Administrator@bfyang5130>
2015-11-27 14:07:19 +0800

    去掉搜索功能显示

--- backend/views/layouts/navbar.php
+++ backend/views/layouts/navbar.php
@@ -20,11 +20,15 @@ $ul_get = \Yii::$app->request->get();
             </div>
             <div id="top-panel" class="col-xs-12 col-sm-10">
                 <div class="row">
+                    
                     <div class="col-xs-8 col-sm-4">
+                        <?/**
                         <div id="search">
                             <input type="text" placeholder="search"/>
                             <i class="fa fa-search"></i>
                         </div>
+                         * * 8/
+                     */?>
                     </div>
                     <div class="col-xs-4 col-sm-8 top-panel-right">
                         <a href="#" class="about">系统介绍</a>

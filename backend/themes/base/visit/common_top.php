5a71b769d4283947f977206ae399044383d0b678
Administrator <Administrator@bfyang5130>
2016-02-02 15:47:57 +0800

    谁把配置文件也放进去了。

--- backend/themes/base/visit/common_top.php
+++ backend/themes/base/visit/common_top.php
@@ -5,10 +5,8 @@ use yii\helpers\Url;
 <div class="panel-heading">
     <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
         <div class="btn-group" role="group" aria-label="First group">
-            <a href="<?= Url::toRoute('/site/index') ?>" class="btn btn-default">错误日志列表</a>
-            <a href="<?= Url::toRoute('/site/trace') ?>" class="btn btn-default">跟踪日志列表</a>
-            <a href="<?= Url::toRoute('/site/sql') ?>" class="btn btn-default">数据库统计</a>
-            <a href="<?= Url::toRoute('/site/doing') ?>" class="btn btn-default">视图统计</a>
+            <a href="<?= Url::toRoute('/visit/index') ?>" class="btn btn-default">Nginx日志</a>
+            <a href="<?= Url::toRoute('/visit/iis') ?>" class="btn btn-default">IIS日志</a>
         </div>
     </div>
 </div>

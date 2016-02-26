9bb7a0100c475e8857c37cbdda50c0777780a8aa
Administrator <Administrator@bfyang5130>
2016-02-26 10:56:38 +0800

    首页去掉sql

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -3,15 +3,12 @@
 namespace backend\controllers;
 
 use backend\models\ErrorLogSearch;
-use backend\models\SqlLogSearch;
 use backend\services\ErrorLogService;
 use backend\services\TraceLogService;
-use backend\services\SqlTraceService;
 use backend\services\ToolService;
 use Yii;
 use yii\data\Sort;
 use yii\filters\AccessControl;
-use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 use common\models\LoginForm;
 use yii\filters\VerbFilter;
@@ -90,56 +87,6 @@ class SiteController extends Controller {
         return $this->render('errorgraph', ['appnames' => $appnames, 'series' => array($series)]);
     }
 
-    /**
-     * 数据库相关的统计
-     */
-    public function actionSqlgraph() {
-        $page = Yii::$app->request->get("page");
-        if (empty($page)) {
-            $page = 0;
-        }
-        $pre_page = $page - 1;
-        $next_page = $page + 1;
-
-        $search_date = Yii::$app->request->get("search_date");
-        if(empty($search_date)){
-            $search_date = date("Y-m-d");
-        }
-        $day_data = SqlTraceService::getSqlDayGraph($page, $search_date);
-        if (empty($day_data)) {
-            return $this->render('sqlgraph', [
-                        'search_date' => $search_date,
-                        "pre_page" => $pre_page,
-                        "next_page" => $next_page
-            ]);
-        }
-        $appnames = $day_data["appnames"];
-        $series['name'] = $day_data["search_date"] . "访问统计";
-        $series['data'] = $day_data['data']["totalVisit"];
-        $series['dataLabels']['enabled'] = true;
-        $series1['name'] = $day_data["search_date"] . "每秒访问频率";
-        $series1['data'] = $day_data['data']["totalsecondVisit"];
-        $series1['dataLabels']['enabled'] = true;
-        $appnameshourshow = $day_data['data']["hourshow"];
-        $series2 = $day_data['data']["reline24Visit"];
-        $series3 = $day_data['data']["reline24VisitSc"];
-        $series4 = $day_data['data']["reline24Time"];
-        $series5 = $day_data['data']["reline24Timesec"];
-        return $this->render('sqlgraph', [
-                    'search_date' => $search_date,
-                    "appnames" => $appnames,
-                    "appnameshourshow" => $appnameshourshow,
-                    "series" => array($series),
-                    "series1" => array($series1),
-                    "series2" => $series2,
-                    "series3" => $series3,
-                    "series4" => $series4,
-                    "series5" => $series5,
-                    "pre_page" => $pre_page,
-                    "next_page" => $next_page
-        ]);
-    }
-
     public function actionIndex() {
         $params = Yii::$app->request->get();
         $searchModel = new ErrorLogSearch();
@@ -164,24 +111,9 @@ class SiteController extends Controller {
     }
 
     /**
-     * 数据库信息
+     * 登录入口
+     * @return type
      */
-    public function actionSql() {
-        $params = Yii::$app->request->get();
-        $searchModel = new SqlLogSearch();
-        $dataProvider = $searchModel->search($params);
-        $query = $dataProvider->query;
-        $sort = new Sort([
-            'attributes' => [
-                'executedate',
-            ],
-            'defaultOrder' => ['executedate' => SORT_DESC]
-        ]);
-        $locals = ToolService::getPagedRows($query, ['orderBy' => $sort->orders, 'pageSize' => 10]);
-        $locals['searchModel'] = $searchModel;
-        return $this->render('sql', $locals);
-    }
-
     public function actionLogin() {
         $this->layout = 'login';
         if (!\Yii::$app->user->isGuest) {
--- backend/themes/base/site/common_top.php
+++ backend/themes/base/site/common_top.php
@@ -9,7 +9,6 @@ $id = \Yii::$app->controller->action->id;
         <div class="btn-group" role="group" aria-label="First group">
             <a href="<?= Url::toRoute('/site/index') ?>" class="btn btn-default<?= strpos($id, 'index')!==FALSE ? ' btn-primary' : '' ?>">错误日志列表</a>
             <a href="<?= Url::toRoute('/site/trace') ?>" class="btn btn-default<?= strpos($id, 'trace')!==FALSE ? ' btn-primary' : '' ?>">跟踪日志列表</a>
-            <a href="<?= Url::toRoute('/site/sql') ?>" class="btn btn-default<?= strpos($id, 'sql')!==FALSE ? ' btn-primary' : '' ?>">数据库统计</a>
             <a href="<?= Url::toRoute('/site/doing') ?>" class="btn btn-default<?= strpos($id, 'doing')!==FALSE ? ' btn-primary' : '' ?>">视图统计</a>
         </div>
     </div>

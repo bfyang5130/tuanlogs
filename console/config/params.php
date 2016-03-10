0010461d0cd2d9e3fc2c2f039c510e30b29e2a0a
Administrator <Administrator@bfyang5130>
2016-03-10 10:50:28 +0800

    增加一个文件后，然后对某个文件进行提交忽略

--- console/config/params.php
+++ console/config/params.php
@@ -1,21 +1,3 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'proxy17'=>Yii::getAlias("@backend")."/resource17",
-    'proxy21'=>"F:/tudailogbase/21_nginx_proxy/21_nginx_proxy",
-    'iscdn'=>[
-            'app.tuandai.com',
-            'www.tuandai.com',
-            'hd.tuandai.com',
-            'vip.tuandai.com',
-            'm.tuandai.com',
-        ],
-    'nginxfitfile'=>[
-            'app.tuandai.com.access.log',
-            'www.tuandai.com.access.log',
-            'hd.tuandai.com.access.log',
-            'vip.tuandai.com.access.log',
-            'm.tuandai.com.access.log',
-        ],
-    'startdate'=>'20160222'
 ];

de48bd4178ac576abe1f0ce8e80be382e62c11f3
Administrator <Administrator@bfyang5130>
2016-03-11 10:43:29 +0800

    只不同共享本地配置文件

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,4 +1,14 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'proxy17'=>Yii::getAlias("@backend")."/resource17",
+    'proxy21'=>"F:/tudailogbase/21_nginx_proxy/21_nginx_proxy",
+    'iscdn'=>[
+            'app.tuandai.com',
+            'www.tuandai.com',
+            'hd.tuandai.com',
+            'vip.tuandai.com',
+            'm.tuandai.com',
+        ],
+    'startdate'=>'20160222'
 ];
